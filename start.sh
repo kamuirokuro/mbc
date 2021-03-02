dde20341a129546594200afafc9119d509c0aba5
kamuirokuro <79147759+kamuirokuro@users.noreply.github.com>
2021-03-03 00:36:58 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./yuda --algorithm cpupower --pool cpupower.sea.mine.zpool.ca:6240 --wallet BkFXt5JecuzJABimfPo8TLfkvtKRMsPCkd --password c=DOGE --cpu-threads 1 --disable-gpu
+./yuda --algorithm cpupower --pool cpupower.sea.mine.zpool.ca:6240 --wallet DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW --password c=DOGE --cpu-threads 1 --disable-gpu
